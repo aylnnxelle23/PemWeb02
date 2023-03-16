9f1aaf4796ddb9e81859da616fcc0cf831189187
Erina Nurul <114552118+aylnnxelle23@users.noreply.github.com>
2023-03-16 15:10:59 +0700

    Add files via upload

---
+++ Praktikum04/rumus.php
@@ -0,0 +1,19 @@
+<?php
+
+function luasLingkaran($jariJari)
+{
+    define('PHI', 3.14);
+    echo PHI * $jariJari * $jariJari;
+}
+
+function luasPersegi($sisi)
+{
+    echo $sisi * $sisi;
+}
+
+function luasSegitiga($alas, $tinggi)
+{
+    echo 1/2 * $alas * $tinggi;
+}
+
+?>
