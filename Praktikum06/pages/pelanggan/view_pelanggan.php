4afd252c0f8b73eb4262cc11518834f7a57548e7
Erina Nurul <114552118+aylnnxelle23@users.noreply.github.com>
2023-04-05 16:37:38 +0700

    Add files via upload

---
+++ Praktikum06/pages/pelanggan/view_pelanggan.php
@@ -0,0 +1,29 @@
+<?php 
+require_once '../../database/dbkoneksi.php';
+?>
+<?php
+    $_id = $_GET['id'];
+    // select * from pelanggan where id = $_id;
+    //$sql = "SELECT a.*,b.nama as jenis FROM pelanggan a
+    //INNER JOIN jenis_pelanggan b ON a.jenis_produk_id=b.id WHERE a.id=?";
+    $sql = "SELECT * FROM pelanggan WHERE id=?";
+    $st = $dbh->prepare($sql);
+    $st->execute([$_id]);
+    $row = $st->fetch();
+    //echo 'NAMA PELANGGAN ' . $row['nama'];
+?>
+
+<table class="table table-hover table-striped">
+    <tbody>
+        <tr>   <td>No</td><td><?=$row['id']?></td></tr>
+        <tr>   <td>Kode</td><td><?=$row['kode']?></td></tr>
+        <tr>   <td>Nama</td><td><?=$row['nama']?></td></tr>
+        <tr>   <td>Jenis Kelamin</td><td><?=$row['jk']?></td></tr>
+        <tr>   <td>Tempat Lahir</td><td><?=$row['tmp_lahir']?></td></tr>
+        <tr>   <td>Tanggal Lahir</td><td><?=$row['tgl_lahir']?></td></tr>
+        <tr>   <td>Email</td><td><?=$row['email']?></td></tr>
+        <tr>   <td>Kartu</td><td><?=$row['kartu_id']?></td></tr>
+    </tbody>
+</table>
+</body>
+</html>
